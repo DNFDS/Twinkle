828ccfe446413ff97a42de6653409b1280d619cc
DNFDS <799170198@qq.com>
2018-11-28 15:17:01 +0800

    mymusic_UI_demo

---
+++ app/src/main/java/com/example/twinkle/FgFindmusicFragment.java
@@ -0,0 +1,23 @@
+package com.example.twinkle;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.twinkle.R;
+
+public class FgFindmusicFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                            Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fg_friends,container,false);
+    }
+
+    @Override
+    public void onViewCreated(View view,Bundle savedInstanceState){
+        super.onViewCreated(view,savedInstanceState);
+    }
+}
